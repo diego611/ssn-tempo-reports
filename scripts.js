df4a67d20640c14bbe8f3d53c9964ef61e8407d7
Xavi Blanch <xavi.blanch@season.es>
2017-04-09 13:15:55 +0200

    better date format, hide buttons in print version

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
   $('tr.foot.total-level-0:last-child th:nth(2)').remove();
   $('tr.foot.total-level-0:last-child th.nav.total').attr('colspan',3);
+
+
+  var reportDate = $('.header-print-date').text().match(/\(([^)]+)\)/)[1];
+  $('.header-print-date').text(reportDate);
+
 });
